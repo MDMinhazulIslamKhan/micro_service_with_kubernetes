e365b794e6cfb64c100d8d3238d30127a73dac41
MD. Minhazul Islam <minhazulislamkhan@gmail.com>
2025-08-01 14:58:24 +0600

    init: initial setup.

---
+++ service1/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+const data = [];
+
+app.get("/", (req, res) => {
+  res.send("Hello from Service 1!");
+});
+
+app.get("/data", (req, res) => {
+  res.json(data);
+});
+
+app.post("/data", (req, res) => {
+  const newData = req.body;
+  data.push(newData);
+  res.status(201).json(newData);
+});
+
+app.post("/send-to-2", async (req, res) => {
+  try {
+    const response = await fetch("http://localhost:3002/data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req.body),
+    });
+    const responseData = await response.json();
+    res.json(responseData);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to send data to Service 2" });
+  }
+});
+
+app.post("/send-to-3", async (req, res) => {
+  try {
+    const response = await fetch("http://localhost:3003/data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req.body),
+    });
+    const responseData = await response.json();
+    res.json(responseData);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to send data to Service 3" });
+  }
+});
+
+app.get("/stop", (req, res) => {
+  res.send("Stopping server");
+  process.exit();
+});
+
+app.listen(port, () => {
+  console.log(`Service 1 listening at http://localhost:${port}`);
+});
---
+++ service2/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const port = 3002;
+
+app.use(express.json());
+
+const data = [];
+
+app.get("/", (req, res) => {
+  res.send("Hello from Service 2!");
+});
+
+app.get("/data", (req, res) => {
+  res.json(data);
+});
+
+app.post("/data", (req, res) => {
+  const newData = req.body;
+  data.push(newData);
+  res.status(201).json(newData);
+});
+
+app.post("/send-to-1", async (req, res) => {
+  try {
+    const response = await fetch("http://localhost:3001/data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req.body),
+    });
+    const responseData = await response.json();
+    res.json(responseData);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to send data to Service 1" });
+  }
+});
+
+app.post("/send-to-3", async (req, res) => {
+  try {
+    const response = await fetch("http://localhost:3003/data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req.body),
+    });
+    const responseData = await response.json();
+    res.json(responseData);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to send data to Service 3" });
+  }
+});
+
+app.get("/stop", (req, res) => {
+  res.send("Stopping server");
+  process.exit();
+});
+
+app.listen(port, () => {
+  console.log(`Service 2 listening at http://localhost:${port}`);
+});
